6403f6e1137e6b433593d6d58b04b336a5f8b311
janje <serbiansolutions@gmail.com>
2021-12-26 14:25:05 +0100

    hover for full image

--- src/CardImage.svelte
+++ src/CardImage.svelte
@@ -2,15 +2,31 @@
     export let url = 0;
     let show = true;
 </script>
-<img loading="lazy" on:error={() => show = false} class="my-2 {show ? 'show' : 'hide'}"
-     src={'/images/' + url + '.jpg'}>
+<div style="width: 450px; height: 480px" class="m-2 bg-gray-900">
+    <img loading="lazy" on:error={() => show = false} class="{show ? 'show' : 'hide'}"
+         src={'/images/' + url + '.jpg'}>
+</div>
 <style>
     .show {
         object-fit: cover;
         width: 450px;
         height: 480px;
-        box-shadow: #8a2357 5px 4px 5px;
-        border-radius: 5px;
+        box-shadow: #8a2357 0 0 20px;
+        border-radius: 10px;
+        z-index: 10;
+    }
+
+    .show:hover {
+        animation: test 0.7s ease-in-out both;
+    }
+
+    @keyframes test {
+        from {
+            object-fit: cover;
+        }
+        to {
+            object-fit: scale-down;
+        }
     }
 
     .hide {
